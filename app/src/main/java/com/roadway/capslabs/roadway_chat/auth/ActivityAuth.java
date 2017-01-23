ab0fdadd8fefef57158e518b3a4cd0e6742ea871
Konstantin Ermakov <bestwick91@gmail.com>
2017-01-23 22:46:17 +0300

    fix permitions

--- app/src/main/java/com/roadway/capslabs/roadway_chat/auth/ActivityAuth.java
+++ app/src/main/java/com/roadway/capslabs/roadway_chat/auth/ActivityAuth.java
@@ -2,11 +2,13 @@ package com.roadway.capslabs.roadway_chat.auth;
 
 import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.roadway.capslabs.roadway_chat.R;
 
@@ -50,5 +52,32 @@ public class ActivityAuth extends AppCompatActivity  {
     @Override
     protected void onPostResume() {
         super.onPostResume();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(ActivityAuth.this, "Разрешения не получены", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 }
